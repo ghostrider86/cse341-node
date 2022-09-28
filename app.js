d21ea418da0bfc941930984ae14a0a0200d4d0aa
CIT 230 <elz20002@byui.edu>
2022-09-28 17:08:53 -0600

    personal_project 5

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ app
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     next();
-    // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-key');
-    // next();
-    // res.setHeader('Content-Type', 'application/json');
-    // next();
-    // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    // next();
+      // res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Z-key');
+      // next();
+      // res.setHeader('Content-Type', 'application/json');
+      // next();
+      // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+      // next();
   })
   .use('/', require('./routes'));
 
